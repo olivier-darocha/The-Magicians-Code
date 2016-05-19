11f3eea5c1733ded8e16e2e1a3be797fd2e27336
Haganeno <darochaolivier@gmail.com>
2016-05-19 11:46:14 +0200

    Version mise à jour
    Fusion des 2 versions.

---
+++ MainProject/Assets/Script/Daycycle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Daycycle : MonoBehaviour {
+
+    public float speed;
+    public Material[] skies;
+
+	void Update () {
+        transform.rotation = Quaternion.Euler(Time.time*speed%360,270,0);
+        if ((name == "Sun" && transform.rotation.eulerAngles.x > 180) ||(name == "Moon" && transform.rotation.eulerAngles.x <= 180))
+        {
+            transform.GetChild(0).GetComponent<Light>().intensity = 0.05f;
+        }
+        else transform.GetChild(0).GetComponent<Light>().intensity = 2f;
+
+        if (name == "Sun" && transform.rotation.eulerAngles.y == 270 && transform.rotation.eulerAngles.x < 30)
+        {
+            RenderSettings.skybox = skies[4];
+        }
+        else if (name == "Sun" && ((transform.rotation.eulerAngles.y == 270 && transform.rotation.eulerAngles.x > 30 && transform.rotation.eulerAngles.x <= 90) || (transform.rotation.eulerAngles.y == 90 && transform.rotation.eulerAngles.x > 30 && transform.rotation.eulerAngles.x <= 90)))
+        {
+            RenderSettings.skybox = skies[0];
+        }
+        else if (name == "Sun" && transform.rotation.eulerAngles.y == 90 && transform.rotation.eulerAngles.x <= 30 && transform.rotation.eulerAngles.x > 10)
+        {
+            RenderSettings.skybox = skies[1];
+        }
+        else if (name == "Sun" && transform.rotation.eulerAngles.y == 90 && ((transform.rotation.eulerAngles.x <= 10 && transform.rotation.eulerAngles.x >= 0) || (transform.rotation.eulerAngles.x <= 360 && transform.rotation.eulerAngles.x > 350)))
+        {
+            RenderSettings.skybox = skies[2];
+        }
+        else if (name == "Sun" && transform.rotation.eulerAngles.y == 90 && transform.rotation.eulerAngles.x <= 350)
+        {
+            RenderSettings.skybox = skies[3];
+        }
+    }
+}
--- MainProject/Assets/Script/GameplayMenuSetup.cs
+++ MainProject/Assets/Script/GameplayMenuSetup.cs
@@ -7,28 +7,19 @@ public class GameplayMenuSetup : MonoBehaviour
 {
 
     //Interacting
-    
+
     private GameObject particles;
     private GameObject panel;
     private bool showPanel;
 
-    /*private GameObject glassObject;
-    private GameObject FPSController;*/
-    
 
     //Programming
-    private GameObject panel0;
-    private GameObject panel1;
-    private GameObject panel2;
-    private GameObject panel3;
-    private GameObject[] panels = new GameObject[20];
-    private bool[] showPanels = new bool[20];
+    public GameObject BAO;
 
     // Use this for initialization
     void Start()
     {
         interactionGlassInit();
-        programmingWindowInit();
     }
 
     void Update()
@@ -46,16 +37,15 @@ public class GameplayMenuSetup : MonoBehaviour
                         break;
                 }
             }
-            if (Input.GetKeyUp(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                programmingWindow();
+                BAO.SetActive(!BAO.activeSelf);
             }
         }
 
         else
         {
             hideInteraction();
-            hideProgramming();
         }
     }
 
@@ -67,9 +57,6 @@ public class GameplayMenuSetup : MonoBehaviour
         particles.GetComponent<ParticleSystem>().Stop();
         particles.GetComponent<ParticleSystem>().emissionRate = 200;
         hideInteraction();
-
-        /*FPSController = GameObject.Find("FPSController");
-        glassObject = GameObject.Find("Final_glass_water");*/
     }
 
     void interactionGlass()
@@ -90,144 +77,5 @@ public class GameplayMenuSetup : MonoBehaviour
         {
             child.gameObject.SetActive(showPanel);
         }
-    }
-
-
-
-    //programming part
-    void programmingWindowInit()
-    {
-        panel0Init();
-        panel1Init();
-        panel2Init();
-        panel3Init();
-        panels[0] = panel0;
-        panels[1] = panel1;
-        panels[2] = panel2;
-        panels[3] = panel3;
-    }
-
-    void programmingWindow()
-    {
-        showPanels[0] = !showPanels[0];
-        panelTrigger(showPanels[0], 0);
-        hideAllChildren();
-    }
-
-    public void buttonTrigger(int i)
-    {
-        switch (i)
-        {
-            case 1:
-                if (!showPanels[1])
-                {
-                    hideAllChildren();
-                    showPanels[1] = !showPanels[1];
-                }
-                else hideAllChildren();
-                panelTrigger(showPanels[1], 1);
-                break;
-            case 2:
-                if (!showPanels[2])
-                {
-                    hideAllChildren();
-                    showPanels[2] = !showPanels[2];
-                }
-                else hideAllChildren();
-                panelTrigger(showPanels[2], 2);
-                break;
-            case 3:
-                if (!showPanels[3])
-                {
-                    hideAllChildren();
-                    showPanels[3] = !showPanels[3];
-                }
-                else hideAllChildren();
-                panelTrigger(showPanels[3], 3);
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    private void panelTrigger(bool show, int i)
-    {
-        switch (i)
-        {
-            case 0:
-                panel0.GetComponent<Image>().enabled = show;
-                foreach (Transform child in panel0.transform)
-                {
-                    child.gameObject.SetActive(show);
-                }
-                break;
-            case 1:
-                panel1.GetComponent<Image>().enabled = show;
-                foreach (Transform child in panel1.transform)
-                {
-                    child.gameObject.SetActive(show);
-                }
-                break;
-            case 2:
-                panel2.GetComponent<Image>().enabled = show;
-                foreach (Transform child in panel2.transform)
-                {
-                    child.gameObject.SetActive(show);
-                }
-                break;
-            case 3:
-                panel3.GetComponent<Image>().enabled = show;
-                foreach (Transform child in panel3.transform)
-                {
-                    child.gameObject.SetActive(show);
-                }
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    void hideProgramming()
-    {
-        showPanels[0] = false;
-        panelTrigger(false, 0);
-        hideAllChildren();
-    }
-
-    private void hideAllChildren()
-    {
-
-        for (int j = 1; j < panels.Length; j++)
-        {
-            panelTrigger(false, j);
-            showPanels[j] = false;
-        }
-    }
-
-    private void panel0Init()
-    {
-        panel0 = GameObject.Find("Panel 0");
-        showPanels[0] = false;
-        panelTrigger(false, 0);
-    }
-    private void panel1Init()
-    {
-        panel1 = GameObject.Find("Panel 1");
-        showPanels[1] = false;
-        panelTrigger(false, 1);
-    }
-    private void panel2Init()
-    {
-        panel2 = GameObject.Find("Panel 2");
-        showPanels[2] = false;
-        panelTrigger(false, 2);
-    }
-    private void panel3Init()
-    {
-        panel3 = GameObject.Find("Panel 3");
-        showPanels[3] = false;
-        panelTrigger(false, 3);
     }
 }
