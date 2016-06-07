780c702f33ff7b09261797006b0920fabec913cd
magnanc <cedricmagnan@laposte.net>
2016-06-07 16:26:14 +0200

    +Drag&Drop

---
+++ MainProject/Assets/GUI/Icons/DragHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject item;    // i changed itembeigdraged to item.
+
+    public GameObject DragWindow;
+    Transform startParent;
+    Vector3 startPosition;
+    bool start = true;
+    //Sprite sprite;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        item = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        item.GetComponent<LayoutElement>().ignoreLayout = true;
+        item.transform.SetParent(DragWindow.transform);
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        item.GetComponent<LayoutElement>().ignoreLayout = false;
+        item = null;
+
+        if (transform.parent == startParent || transform.parent == DragWindow.transform)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+        }
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+    }
+
+}
---
+++ MainProject/Assets/GUI/Icons/ScrollViewHeightFitter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollViewHeightFitter : MonoBehaviour {
+
+    public GameObject Scrollbar;
+    public float contentSize;
+    public float contentPadding;
+    public float contentSpacing;
+    private int done;
+
+    void Start()
+    {
+        done = (int)(this.GetComponent<RectTransform>().rect.width / contentSize);
+        int i = transform.childCount;
+        if(done != 1) this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y / (done / (done - 1)));
+    }
+
+    public void ChangeSize()
+    {
+        int i = transform.childCount;
+        if (contentSize * 4 + 2 * contentPadding + 3*contentSpacing > this.GetComponent<RectTransform>().rect.width && done==4)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y * (done/(done-1)));
+            done = 3;
+        }
+        else if (contentSize * 3 + 2 * contentPadding + 2*contentSpacing > this.GetComponent<RectTransform>().rect.width && done==3)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y * (done / (done - 1)));
+            done = 2;
+        }
+        else if (contentSize*2 + 2*contentPadding + contentSpacing > this.GetComponent<RectTransform>().rect.width && done==2)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y * (done / (done - 1)));
+            done = 1;
+        }
+        else if(done==1 && contentSize * 2 + 2 * contentPadding + contentSpacing <= this.GetComponent<RectTransform>().rect.width)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y / ((done+1)/done));
+            done = 2;
+        }
+        else if (done == 2 && contentSize * 3 + 2 * contentPadding + 2*contentSpacing <= this.GetComponent<RectTransform>().rect.width)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y / ((done+1)/done));
+            done = 3;
+        }
+        else if (done == 3 && contentSize * 4 + 2 * contentPadding + 3*contentSpacing <= this.GetComponent<RectTransform>().rect.width)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(-20, this.GetComponent<RectTransform>().sizeDelta.y / ((done+1)/done));
+            done = 4;
+        }
+    }
+}
